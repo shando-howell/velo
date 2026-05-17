ec6af5263895245bc0b4091218111fab387f7290
shandohowell <shandohowell94@gmail.com>
2026-05-16 22:59:33 -0500

    Updated dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
                 <div className="flex-1 uppercase text-yellow-600 text-xl">
                     <h2>Dashboard</h2>
                 </div>
-                <div>
+                <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row">
                     <Link href="/admin/dashboard/appointments" className="p-3 bg-yellow-600 hover:bg-yellow-500 text-white">
                         Manage Appointments
                     </Link>
